389c341999a912aa4230d8c769503a92fff57db3
mmk <mmkdhrubo@gmail.com>
2023-08-03 05:43:57 +0600

    writing envs using python

--- create_env.py
+++ create_env.py
@@ -3,7 +3,8 @@ import os
 
 def create_env_file():
     with open(".env", "w") as f:
-        f.write(f"PROJECT_NAME={os.getenv('_PROJECT_NAME')}\n")
+        f.write(f"PROJECT_NAME={os.getenv('This is a project name')}\n")
+        # f.write(f"PROJECT_NAME={os.getenv('_PROJECT_NAME')}\n")
         f.write(f"VERSION_NAME={os.getenv('_VERSION_NAME')}\n")
         f.write(f"SHORT_SHA={os.getenv('_SHORT_SHA')}\n")
         f.write(f"BRANCH_NAME={os.getenv('_BRANCH_NAME')}\n")

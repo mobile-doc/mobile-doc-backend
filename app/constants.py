13708bf6986c9b94bbd0a756d4cc7da0a7c40820
mmk <mmkdhrubo@gmail.com>
2023-08-04 03:10:10 +0600

    back to stackoverflow soln for env_vars

--- app/constants.py
+++ app/constants.py
@@ -11,15 +11,5 @@ VERSION_NAME = os.getenv("GAE_VERSION")
 VERSION_NAME = "dev" if not VERSION_NAME else VERSION_NAME
 GOOGLE_CLOUD_PROJECT = os.getenv("GOOGLE_CLOUD_PROJECT")
 
-try:
-    for root, _, files in os.walk(os.getcwd()):
-        for file in files:
-            if file == "credential.json":
-                credential_file_path = os.path.join(root, file)
-
-    with open(credential_file_path, "r") as file:
-        data = json.load(file)
-        atlas_password = data.get("_ATLAS_PASSWORD")
-        atlas_username = data.get("_ATLAS_USERNAME")
-except Exception as e:
-    logging.error("Couldn't read from 'credential.json': %s", str(e))
+atlas_password = os.getenv("_ATLAS_PASSWORD")
+atlas_username = os.getenv("_ATLAS_USERNAME")

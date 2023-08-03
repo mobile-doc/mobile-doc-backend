d392e8d9f3ae144a2a47729ce8cc7b800eb313d5
mmk <mmkdhrubo@gmail.com>
2023-08-04 02:34:38 +0600

    debugging for finding credential.json

--- app/constants.py
+++ app/constants.py
@@ -1,6 +1,7 @@
 import os
 from dotenv import load_dotenv
 import json
+import logging
 
 # Load environment variables from .env
 load_dotenv()
@@ -10,7 +11,14 @@ VERSION_NAME = os.getenv("GAE_VERSION")
 VERSION_NAME = "dev" if not VERSION_NAME else VERSION_NAME
 GOOGLE_CLOUD_PROJECT = os.getenv("GOOGLE_CLOUD_PROJECT")
 
-with open("credential.json", "r") as file:
-    data = json.load(file)
-    atlas_password = data.get("_ATLAS_PASSWORD")
-    atlas_username = data.get("_ATLAS_USERNAME")
+try:
+    with open("credential.json", "r") as file:
+        data = json.load(file)
+        atlas_password = data.get("_ATLAS_PASSWORD")
+        atlas_username = data.get("_ATLAS_USERNAME")
+except:
+    logging.log("Couldn't read from 'credential.json'")
+    for root, _, files in os.walk(os.getcwd()):
+        for file in files:
+            file_path = os.path.join(root, file)
+            print(file_path)
